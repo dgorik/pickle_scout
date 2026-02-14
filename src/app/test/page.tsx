ff9f758c8827f4d383fa7bd374d594e3edb6f5ed
Daniel Gorbachev <dg186533@gmail.com>
2026-02-14 16:21:51 -0500

    added comments to react testing

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,46 +2,91 @@
 
 import { useEffect, useState } from "react";
 
-type Favs = {
-  shape: string;
-  color: string;
-  animal: string;
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 };
 
-type User = {
-  id: string;
-  name: string;
-  favorites: Favs;
+type Comment = {
+  id: number;
+  email: string;
+  body: string;
 };
+
+const post_url = "https://jsonplaceholder.typicode.com/posts?_limit=5";
 
 export default function Test() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setErrors] = useState("");
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [postsLoading, setPostsLoading] = useState<boolean>(true);
+  const [commentsLoading, setCommentsLoading] = useState<boolean>(false);
+  const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
+  const [comments, setComments] = useState<Comment[]>([]);
 
-  const url = "https://example.gcommer.com/users";
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchPosts = async () => {
       try {
-        const res = await fetch(url);
+        const res = await fetch(post_url);
         const data = await res.json();
-        setUsers(data);
-      } catch (e) {
-        setErrors("Error");
+        setPosts(data);
+      } finally {
+        setPostsLoading(false);
       }
     };
-    fetchUsers();
+    setTimeout(() => {
+      fetchPosts();
+    }, 2000);
   }, []);
 
-  if (error) return <p> {error} </p>;
+  useEffect(() => {
+    if (!selectedPostId) return;
+    setCommentsLoading(true);
+    setComments([]);
+    const fetchComments = async () => {
+      try {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${selectedPostId}/comments`,
+        );
+        const data = await res.json();
+        setComments(data);
+      } finally {
+        setCommentsLoading(false);
+      }
+    };
+    setTimeout(() => {
+      fetchComments();
+    }, 2000);
+  }, [selectedPostId]);
+
+  if (postsLoading) return " Posts are Loading please wait";
+
+  if (commentsLoading) return " Comments are Loading please wait";
 
   return (
-    <div>
-      <p> Hello there</p>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.name} = {user.favorites.shape}
-        </li>
-      ))}
+    <div className="flex flex-col">
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <button
+              className="bg-blue-200"
+              onClick={() => setSelectedPostId(post.id)}
+            >
+              {post.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {selectedPostId && (
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment.id}>
+              <p> {comment.email}</p>
+              <p> {comment.body}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
